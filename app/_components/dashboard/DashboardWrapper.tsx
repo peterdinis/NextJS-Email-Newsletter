f5ea1dcb88d477a90c02c065a9950721fe7717d7
Peter Dinis <peterdinis0@pobox.sk>
2024-03-02 20:06:48 +0100

    FOO

--- app/_components/dashboard/DashboardWrapper.tsx
+++ app/_components/dashboard/DashboardWrapper.tsx
@@ -1,10 +1,13 @@
 import { UserButton } from '@clerk/nextjs';
 import { FC } from 'react';
+import AppLayout from '../shared/AppLayout';
 
 const DashboardWrapper: FC = () => {
-    return <>
-        <UserButton />
-    </>;
+    return (
+        <AppLayout>
+            DASHBOARD
+        </AppLayout>
+    )
 };
 
 export default DashboardWrapper;
