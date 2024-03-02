3956b8def5805d62f93be914acc8d6272c8e65ed
Peter Dinis <peterdinis0@pobox.sk>
2024-03-02 17:44:37 +0100

    feat: Updating landing page WIP

--- app/_components/hero/HeroWrapper.tsx
+++ app/_components/hero/HeroWrapper.tsx
@@ -1,31 +1,72 @@
 import { FC } from 'react';
-import { Spotlight } from '../shared/Spootlight';
-import { Button } from '@nextui-org/react';
-
 const HeroWrapper: FC = () => {
     return (
         <>
-            <div className='h-[40rem] w-full rounded-md flex md:items-center md:justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden'>
-                <Spotlight
-                    className='-top-40 left-0 md:left-60 md:-top-20'
-                    fill='black'
-                />
-                <div className=' p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0'>
-                    <h1 className='text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-black'>
-                        Send Emails <br /> right way
-                    </h1>
-                    <p className='mt-4 font-normal text-base text-black max-w-lg text-center mx-auto'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Placeat excepturi id velit aperiam similique est
-                        sapiente ipsum, provident rem alias. Lorem ipsum dolor
-                        sit amet consectetur adipisicing elit. Ipsam doloribus
-                        consectetur, fuga soluta maxime error nihil. Hic
-                        doloribus expedita quam!
-                    </p>
-
-                    <div className='mt-5 flex justify-center'>
-                        <Button color="primary" variant="shadow">Login and try</Button>
+            <div className='relative isolate lg:px-8'>
+                <div
+                    className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+                    aria-hidden='true'
+                >
+                    <div
+                        className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                    />
+                </div>
+                <div className='mx-auto max-w-2xl sm:py-48 lg:py-56'>
+                    <div className='hidden sm:mb-8 sm:flex sm:justify-center'>
+                        <div className='relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
+                            Announcing our next round of funding.{' '}
+                            <a
+                                href='#'
+                                className='font-semibold text-indigo-600'
+                            >
+                                <span
+                                    className='absolute inset-0'
+                                    aria-hidden='true'
+                                />
+                                Read more <span aria-hidden='true'>&rarr;</span>
+                            </a>
+                        </div>
                     </div>
+                    <div className='text-center'>
+                        <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+                            Data to enrich your online business
+                        </h1>
+                        <p className='mt-6 text-lg leading-8 text-gray-600'>
+                            Anim aute id magna aliqua ad ad non deserunt sunt.
+                            Qui irure qui lorem cupidatat commodo. Elit sunt
+                            amet fugiat veniam occaecat fugiat aliqua.
+                        </p>
+                        <div className='mt-10 flex items-center justify-center gap-x-6'>
+                            <a
+                                href='#'
+                                className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                            >
+                                Get started
+                            </a>
+                            <a
+                                href='#'
+                                className='text-sm font-semibold leading-6 text-gray-900'
+                            >
+                                Learn more <span aria-hidden='true'>→</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'
+                    aria-hidden='true'
+                >
+                    <div
+                        className='relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                    />
                 </div>
             </div>
         </>
