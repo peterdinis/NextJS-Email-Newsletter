0b382e8e0260d87debee9f884f3e115531727005
Peter Dinis <peterdinis0@pobox.sk>
2024-03-02 20:15:27 +0100

    FOO

--- app/_components/dashboard/DashboardWrapper.tsx
+++ app/_components/dashboard/DashboardWrapper.tsx
@@ -1,11 +1,19 @@
-import { UserButton } from '@clerk/nextjs';
+"use client"
+
+
 import { FC } from 'react';
 import AppLayout from '../shared/AppLayout';
+import { useUser } from '@clerk/nextjs';
 
 const DashboardWrapper: FC = () => {
+    const {user} = useUser();
     return (
         <AppLayout>
-            DASHBOARD
+            <div className='p-5 w-full'>
+                <h1 className='text-3xl text-black font-medium'>
+                    Hi {user?.fullName}
+                </h1>
+            </div>
         </AppLayout>
     )
 };
---
+++ app/_components/shared/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Sidebar: FC = () => {
+    return (
+        <div className='w-[290px] h-screen overflow-y-scroll'>
+            Sidebar
+        </div>
+    )
+}
+
+export default Sidebar;
