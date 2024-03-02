183649a6f38fa0a347440258c1046f0882e5ed2a
Peter Dinis <peterdinis0@pobox.sk>
2024-03-02 19:08:36 +0100

    feat: Adding clerk WIP

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
-import DashboardWrapper from '@/components/dashboard/DashboardWrapper';
 import { NextPage } from 'next';
+import DashboardWrapper from '../_components/dashboard/DashboardWrapper';
 
 const DashboardPage: NextPage = () => {
     return <DashboardWrapper />;
