7f9d27cec7d914a9aafc07ae710a2de29d149d8d
Peter Dinis <peterdinis0@pobox.sk>
2024-03-02 20:03:55 +0100

    FOO

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 import { NextPage } from 'next';
 import HeroWrapper from './_components/hero/HeroWrapper';
-import HeroServices from './_components/hero/HeroServices';
-import AuthHelper from './_components/shared/AuthHelper';
 
 const Homepage: NextPage = () => {
     return (
         <>
             <HeroWrapper />
-            <AuthHelper>
-                <HeroServices />
-            </AuthHelper>
         </>
     );
 };
