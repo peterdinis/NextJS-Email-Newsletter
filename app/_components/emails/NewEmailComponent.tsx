661871fd42ecd5aa693ce30b8f6c1873e4ad287f
Peter Dinis <peterdinis0@pobox.sk>
2024-03-05 18:09:32 +0100

    FOO

---
+++ app/_components/emails/NewEmailComponent.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const NewEmailComponent: FC = () => {
+    return (
+        <></>
+    )
+}
+
+export default NewEmailComponent;
---
+++ app/emails/new-email/page.tsx
@@ -0,0 +1,8 @@
+import NewEmailComponent from "@/app/_components/emails/NewEmailComponent";
+import { NextPage } from "next";
+
+const NewEmailPage: NextPage = () => {
+    return <NewEmailComponent />
+}
+
+export default NewEmailPage;
