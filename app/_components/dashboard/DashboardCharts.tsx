3a2b21ac11daaf0b4d3f94ecac9bee0baf4d1c20
Peter Dinis <peterdinis0@pobox.sk>
2024-03-05 18:05:29 +0100

    FOO

--- app/_components/dashboard/DashboardCharts.tsx
+++ app/_components/dashboard/DashboardCharts.tsx
@@ -4,41 +4,41 @@ import { Card, AreaChart, Title, Text } from '@tremor/react';
 import { FC } from 'react';
 
 const data = [
-  {
-    Month: 'Jan 21',
-    Sales: 2890,
-    Profit: 2400
-  },
-  {
-    Month: 'Feb 21',
-    Sales: 1890,
-    Profit: 1398
-  },
-  {
-    Month: 'Jan 22',
-    Sales: 3890,
-    Profit: 2980
-  }
+    {
+        Month: 'Jan 21',
+        Sales: 2890,
+        Profit: 2400,
+    },
+    {
+        Month: 'Feb 21',
+        Sales: 1890,
+        Profit: 1398,
+    },
+    {
+        Month: 'Jan 22',
+        Sales: 3890,
+        Profit: 2980,
+    },
 ];
 
 const DashboardCharts: FC = () => {
-  return (
-    <Card className="mt-8">
-      <Title>Performance</Title>
-      <Text>Comparison between Sales and Profit</Text>
-      <AreaChart
-        className="mt-4 h-80"
-        data={data}
-        categories={['Sales', 'Profit']}
-        index="Month"
-        colors={['indigo', 'fuchsia']}
-        valueFormatter={(number: number) =>
-          `$ ${Intl.NumberFormat('us').format(number).toString()}`
-        }
-        yAxisWidth={60}
-      />
-    </Card>
-  );
-}
+    return (
+        <Card className='mt-8'>
+            <Title>Performance</Title>
+            <Text>Comparison between Sales and Profit</Text>
+            <AreaChart
+                className='mt-4 h-80'
+                data={data}
+                categories={['Sales', 'Profit']}
+                index='Month'
+                colors={['indigo', 'fuchsia']}
+                valueFormatter={(number: number) =>
+                    `$ ${Intl.NumberFormat('us').format(number).toString()}`
+                }
+                yAxisWidth={60}
+            />
+        </Card>
+    );
+};
 
 export default DashboardCharts;
