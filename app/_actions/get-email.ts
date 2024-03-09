9611299be60d1a6ef2848a57c99728a544f20ed6
Peter Dinis <peterdinis0@pobox.sk>
2024-03-09 20:40:54 +0100

    FOO

---
+++ app/_actions/get-email.ts
@@ -0,0 +1,18 @@
+import { prisma } from '@/app/_db/prisma-init';
+
+export const getEmails = async ({
+    newsLetterOwnerId,
+  }: {
+    newsLetterOwnerId: string;
+  }) => {
+    try {
+      const emails = await prisma.email.findFirst({ 
+        where: {
+            newLetterOwnerId: newsLetterOwnerId
+        }
+       });
+      return emails;
+    } catch (error) {
+      console.log(error);
+    }
+  };
